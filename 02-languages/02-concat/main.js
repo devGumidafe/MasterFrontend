7235a5d00f3e5b8861e5c66cc7df5d2f8b0eda19
Gumidafe Socorro <devgumidafe@gmail.com>
2021-10-19 11:17:41 +0100

    fixed concat

--- 02-languages/02-concat/main.js
+++ 02-languages/02-concat/main.js
@@ -3,5 +3,5 @@
 const concat = (a, b) => [...a, ...b];
 console.log(concat([1, 2, 3], [4, 5, 6]));
 
-const betterConcat = (...props) => [props.join()];
+const betterConcat = (...props) => [].concat(...props);
 console.log(betterConcat([1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], ['a', 'b', 'c']));
